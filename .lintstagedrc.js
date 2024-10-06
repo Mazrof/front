a755375eb29fcefc102515f1dcad60777f979aca
New-pro125 <ahmed125.am13@gmail.com>
2024-10-06 15:10:34 +0300

    Adding New Dependencies

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-  // Lint & Prettify TS, TSX, JS, JSX files
-  "**/*.(ts|tsx|js|jsx)": [
-      // Lint the files using ESLint
-    `npm run prettier`,  // Prettify the files using Prettier
+  // Lint & Prettify TS, TSX, JS, JSX files within the app directory and its subdirectories, excluding the dist directory
+  "/**/*.{ts,tsx,js,jsx}": [
+    `npm run lint`,
+    `npm run prettier`,
   ],
-  
+  "!dist/**/*": [], // Exclude the dist directory from linting
+  "!node_modules/**/*": [], // Exclude the node_modules directory from linting
+  "!.next/**/*": [], // Exclude the .next directory from linting
+  "!.swc/**/*": [], // Exclude the .swc directory from linting
+   "!.husky/**/*": [], // Exclude the .husky directory from linting
 };
