b4312ec87c6caf3330b6d387742e77077975e771
New-pro125 <ahmed125.am13@gmail.com>
2024-10-06 15:12:37 +0300

    Adding New Dependencies

--- __tests__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "@/app/page";
-
-test("renders learn react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/Deploy Now/i);
-  expect(linkElement).toBeInTheDocument();
-});
