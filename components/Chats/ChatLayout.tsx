dbaaecf7817f2d10f211c502856c565d4743ffa4
New-pro125 <ahmed125.am13@gmail.com>
2024-10-19 23:03:22 +0300

    Message Component Added

--- components/Chats/ChatLayout.tsx
+++ components/Chats/ChatLayout.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import InputMessage from "@/components/Chats/InputMessage"
-function ChatLayout() {
+import InputMessage from "@/components/Chats/InputMessage";
+import {
+    Message,
+    MessageCreatedAt,
+    MessageImage,
+    MessageText,
+    MessageVideo,
+} from "@/components/Chats/Message";
+
+type MessageResponseType = {
+    text: string | undefined;
+    imageUrl: string[] | undefined;
+    createdAt: string | undefined;
+    videoUrl: string[] | undefined;
+};
+async function ChatLayout() {
+    const response = await fetch("http://localhost:4000/messages");
+    const messages: MessageResponseType[] = await response.json();
     return (
-        <div className='dark:bg-dark bg-light hidden md:block w-full min-h-screen overflow-y-auto relative'>
-            Chatting
-            <InputMessage placeHolder='Message'/>
+        <div className="dark:bg-dark bg-light relative hidden min-h-screen w-full overflow-y-auto md:block">
+            <div>
+                {messages.map((message, index) => (
+                    <Message message={message} key={index}>
+                        <MessageImage />
+                        <MessageText />
+                        <MessageVideo />
+                        <MessageCreatedAt />
+                    </Message>
+                ))}
+            </div>
+            <InputMessage placeHolder="Message" />
         </div>
-    )
+    );
 }
 
-export default ChatLayout
+export default ChatLayout;
