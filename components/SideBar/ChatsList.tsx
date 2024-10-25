6045d9cc55442a53eabab8dd262904924cfd0e95
aliAhussain <ali.ali04@eng-st.cu.edu.eg>
2024-10-25 03:36:43 +0300

    add some icons

--- components/SideBar/ChatsList.tsx
+++ components/SideBar/ChatsList.tsx
@@ -1,131 +1,140 @@
 // components/ChatList.js
+"use client";
 import Image from "next/image";
-import React from "react";
-
+import React, { useState } from "react";
+import Avatar from "./Avatar";
 const chatData = [
-  {
-    id: 1,
-    name: "John Doe",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Hey! How are you?",
-    time: "10:30 AM",
-    unreadCount: 2,
-    pinned: true,
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "10:20 PM",
-    unreadCount: 0,
-    pinned: false,
-  },
-  {
-    id: 3,
-    name: "Mike Tyson",
-    avatar: "/images/Avatar.png",
-    lastMessage: "See you tomorrow at 3!",
-    time: "2 days ago",
-    unreadCount: 5,
-    pinned: true,
-  },
-  {
-    id: 4,
-    name: "Nathan",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "Yesterday",
-    unreadCount: 0,
-    pinned: true,
-  },
-  {
-    id: 5,
-    name: "Harith",
-    avatar: "/images/Avatar.png",
-    lastMessage: "I will kill you watch out",
-    time: "Oct 15",
-    unreadCount: 0,
-    pinned: false,
-  },
-  {
-    id: 6,
-    name: "Arther",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "Yesterday",
-    unreadCount: 1,
-    pinned: false,
-  },
-  {
-    id: 7,
-    name: "Ali",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "Yesterday",
-    unreadCount: 5,
-    pinned: false,
-  },
-  {
-    id: 8,
-    name: "Ahmed",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "Sep 28",
-    unreadCount: 0,
-    pinned: false,
-  },
-  {
-    id: 9,
-    name: "Jade",
-    avatar: "/images/Avatar.png",
-    lastMessage: "Got the files. Thanks!",
-    time: "Aug 9",
-    unreadCount: 3,
-    pinned: false,
-  },
+    {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Hey! How are you?",
+        time: "10:30 AM",
+        unreadCount: 2,
+        pinned: true,
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "10:20 PM",
+        unreadCount: 0,
+        pinned: false,
+    },
+    {
+        id: 3,
+        name: "Mike Tyson",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "See you tomorrow at 3!",
+        time: "2 days ago",
+        unreadCount: 5,
+        pinned: true,
+    },
+    {
+        id: 4,
+        name: "Nathan",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "Yesterday",
+        unreadCount: 0,
+        pinned: true,
+    },
+    {
+        id: 5,
+        name: "Harith",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "I will kill you watch out",
+        time: "Oct 15",
+        unreadCount: 0,
+        pinned: false,
+    },
+    {
+        id: 6,
+        name: "Arther",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "Yesterday",
+        unreadCount: 1,
+        pinned: false,
+    },
+    {
+        id: 7,
+        name: "Ali",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "Yesterday",
+        unreadCount: 5,
+        pinned: false,
+    },
+    {
+        id: 8,
+        name: "Ahmed",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "Sep 28",
+        unreadCount: 0,
+        pinned: false,
+    },
+    {
+        id: 9,
+        name: "Jade",
+        avatar: "/images/default-avatar.gif",
+        lastMessage: "Got the files. Thanks!",
+        time: "Aug 9",
+        unreadCount: 3,
+        pinned: false,
+    },
 ];
 
 const ChatList = () => {
-  return (
-    <div className="h-full overflow-y-auto custom-scrollbar space-y-4 p-2">
-      {chatData.map((chat) => (
-        <div
-          key={chat.id}
-          className="flex items-center p-3 bg-[#212121] rounded-lg shadow-sm hover:bg-[#2B2B2B] transition duration-150 cursor-pointer"
-        >
-          <Image
-            src={chat.avatar}
-            alt={chat.name}
-            width={50}
-            height={50}
-            className="rounded-full object-cover"
-          />
-          <div className="ml-4 flex-grow">
-            <div className="flex justify-between items-center">
-              <h3 className="text-lg font-semibold text-white">{chat.name}</h3>
-              <span className="text-xs text-gray-500">{chat.time}</span>
-            </div>
-            <div className="flex justify-between items-center">
-              <p className="text-sm text-gray-400 truncate max-w-36">
-                {chat.lastMessage}
-              </p>
-              <div className="min-w-7 flex items-center space-x-1">
-                {chat.unreadCount > 0 ? (
-                  <span className="ml-2 bg-blue-500 text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full">
-                    {chat.unreadCount}
-                  </span>
-                ) : (
-                  <span className="text-gray-400">✔️</span>
-                )}
-                {chat.pinned && <span>📌</span>}
-              </div>
-            </div>
-          </div>
+    const [hasImage, setHasImage] = useState(true);
+
+    return (
+        <div className="custom-scrollbar h-full space-y-4 overflow-y-auto p-2">
+            {chatData.map((chat, index) => (
+                <div
+                    key={chat.id}
+                    className="flex cursor-pointer items-center rounded-lg bg-[#212121] p-3 shadow-sm transition duration-150 hover:bg-[#2B2B2B]"
+                >
+                    {hasImage && index%2==0 ? (
+                        <Image
+                            src={chat.avatar}
+                            alt={chat.name}
+                            width={50}
+                            height={50}
+                            className="rounded-full object-cover"
+                        />
+                    ) : (
+                        <div className="rounded-full object-cover">
+                            <Avatar name={chat.name} />
+                        </div>
+                    )}
+                    <div className="ml-4 flex-grow">
+                        <div className="flex items-center justify-between">
+                            <h3 className="text-lg font-semibold text-white">{chat.name}</h3>
+                            <span className="text-xs text-gray-500">{chat.time}</span>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <p className="max-w-36 truncate text-sm text-gray-400">
+                                {chat.lastMessage}
+                            </p>
+                            <div className="flex min-w-7 items-center space-x-1">
+                                {chat.unreadCount > 0 ? (
+                                    <span className="ml-2 flex h-5 w-5 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white">
+                                        {chat.unreadCount}
+                                    </span>
+                                ) : (
+                                    <span className="text-gray-400">✔️</span>
+                                )}
+                                {chat.pinned && <span className="hover:bg-slate-600 rounded-full">📌</span>}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default ChatList;
--- components/SideBar/ChatsSearchBar.tsx
+++ components/SideBar/ChatsSearchBar.tsx
@@ -1,46 +1,93 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
 export default function ChatsSearchBar() {
-  return (
-    <div>
-      <header className="flex items-center justify-between bg-[#2C2F33] p-4 shadow-md">
-        <div className="flex items-center">
-          <Image
-            src="/images/logo.jpg"
-            alt="App Logo"
-            width={40}
-            height={40}
-            className="rounded-full"
-          />
-          <h1 className="text-white text-xl font-semibold ml-2">Mazrof</h1>
-        </div>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-        <div className="relative flex-grow mx-4">
-          <input
-            type="text"
-            placeholder="Search"
-            className="w-full bg-[#3E4146] text-white rounded-full py-2 px-4 focus:outline-none"
-          />
-        </div>
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    return (
+        <div>
+            <header className="flex items-center justify-between bg-[#2C2F33] p-4 shadow-md">
+                <div className="flex items-center">
+                    <Image
+                        src="/images/logo.jpg"
+                        alt="App Logo"
+                        width={40}
+                        height={40}
+                        className="rounded-full"
+                    />
+                    <h1 className="ml-2 text-xl font-semibold text-white">Mazrof</h1>
+                </div>
 
-        <div className="flex space-x-4">
-          <button className="text-white hover:text-blue-500 transition duration-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
+                <div className="relative mx-4 flex-grow">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="w-full rounded-full bg-[#3E4146] px-4 py-2 text-white focus:outline-none"
+                    />
+                </div>
+
+                <div className="flex space-x-4">
+                    <button
+                        onClick={toggleMenu}
+                        className="text-white transition duration-200 hover:text-blue-500"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                {isMenuOpen && (
+                    <div className="absolute right-0 top-20 z-10 w-48 rounded-lg bg-[#201f1f] py-2 text-gray-100 shadow-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <ul>
+                            <li className="flex cursor-pointer gap-2 px-4 py-2 hover:bg-[#2B2B2B]">
+                                <Image
+                                    src="/images/settings.gif"
+                                    alt="settings icon"
+                                    width={20}
+                                    height={20}
+                                    className="rounded-full"
+                                />
+                                Settings
+                            </li>
+                            <li className="flex cursor-pointer gap-2 px-4 py-2 hover:bg-[#2B2B2B]">
+                                <Image
+                                    src="/images/save.gif"
+                                    alt="save icon"
+                                    width={20}
+                                    height={20}
+                                    className="rounded-full"
+                                />
+                                Saved Messages
+                            </li>
+                            <li className="flex cursor-pointer gap-2 px-4 py-2 hover:bg-[#2B2B2B]">
+                                <Image
+                                    src="/images/telegram-stories.gif"
+                                    alt="telegram-stories icon"
+                                    width={20}
+                                    height={20}
+                                    className="rounded-full"
+                                />
+                                My Stories
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
