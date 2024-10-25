f907c901842b7b44ad8487c2aee277252a1a54cb
aliAhussain <ali.ali04@eng-st.cu.edu.eg>
2024-10-26 00:41:14 +0300

    delete newchatbtn and add group class to the wrapper div of side bar

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -10,7 +10,7 @@ function SideBar() {
     const isSelectedChat = isSelectedChatId();
     return (
         <div
-            className={`min-w-1/3 relative min-h-screen ${isSelectedChat && "hidden"} max-w-full overflow-y-hidden bg-[#212121] py-2 pl-2 md:block`}
+            className={`min-w-1/3 relative min-h-screen ${isSelectedChat && "hidden"} max-w-full overflow-y-hidden bg-[#212121] py-2 pl-2 md:block group`}
         >
             <ChatsSearchBar />
             <ChatList />
