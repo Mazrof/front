a6ae49162913c64cceb32b8d39ad61a08aec973e
Nesma Osama <128908402+Nesma-Osama@users.noreply.github.com>
2024-10-31 00:49:55 +0300

    first structure of login page and Aouth

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,24 @@
+import { signIn, auth } from "@/auth";
+
+export default async function Login() {
+    const session = await auth();
+    return (
+        <form
+            className="flex h-96 flex-col items-center justify-around "
+            action={async () => {
+                "use server";
+                await signIn("github");
+            }}
+        >   
+            <h1 className="bold text-3xl  text-blue-900">LOGIN</h1>
+            <div className="flex flex-col items-center justify-evenly gap-3">
+                <button type="submit" name="action" value="google" className="auth-buttons">
+                    Login with Google
+                </button>
+                <button type="submit" name="action" value="github" className="auth-buttons">
+                    Login with GitHub
+                </button>
+            </div>
+        </form>
+    );
+}
