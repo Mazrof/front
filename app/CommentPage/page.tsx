3b03f09bd1f255c32838c445ca214625fed76d36
alisamy404@outlook.com <alisamy404@outlook.com>
2024-11-28 23:41:11 +0200

    remove record voice code and build error

--- app/CommentPage/page.tsx
+++ app/CommentPage/page.tsx
@@ -2,18 +2,15 @@
 //import ForwardDialog from "./components/ForwardDialog";
 //import {useState,createContext} from "react";
 //import SelectBox from "./components/SelectBox";
-import React, { Suspense,useState } from 'react';
-import ChatLayout from '@/components/Chats/ChatLayout';
-import ChatRoom from '@/components/Chats/ChatRoom';
-import MessageLoading from '@/components/Chats/Message/MessageLoading';
+import React from 'react';
 import InputMessage from '@/components/Chats/InputMessage/InputMessage';
 import CommentBar from '@/components/Chats/CommentBar';
 
 export default function Home() {
   
-  const [darkMode, setDarkMode] = useState(false);
+  
   return (
-    <div className={`container mx-auto min-w-screen flex max-h-screen max-w-full ${darkMode && "dark"} relative min-h-screen flex-1 overflow-hidden`} id='comment-page'>
+    <div className={`container mx-auto min-w-screen flex max-h-screen max-w-full relative min-h-screen flex-1 overflow-hidden`} id='comment-page'>
 
         <div className='bg-light dark:bg-black flex flex-col items-center'>
             <CommentBar/>
--- components/Chats/CommentBar.tsx
+++ components/Chats/CommentBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {useState} from 'react';
+//import {useState} from 'react';
 import { useRouter } from 'next/navigation';
 import { LeftArrowIcon, SearchIcon } from '@/utils/icons';
 const CommentBar = () => {
     const router = useRouter();
-    const[commentNumber,setCommentNumber]=useState<number>(0);
+    //const[commentNumber,setCommentNumber]=useState<number>(0);
     return (
         <div className="border-2 border-gray-200 bg-white text-black p-4" id='comment-bar'>
             <div className="container mx-auto flex justify-between items-center">
@@ -15,7 +15,7 @@ const CommentBar = () => {
                     <LeftArrowIcon/>
                 </button>
                 <div>
-                    {commentNumber} Comment
+                    0 Comment
                 </div>
                 <button
                     className="mb-4 p-4 bg-gray-600 text-white rounded-lg"
